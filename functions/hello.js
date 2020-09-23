86e38823bf3f09280fef928da772aa9a03c4a23d
Mario Terron <marioterron157@gmail.com>
2020-09-23 23:41:02 +0200

    feat: debugging netlify prod

--- functions/hello.js
+++ functions/hello.js
@@ -1,4 +1,4 @@
-exports.handler = () => {
+exports.handler = (event, context, callback) => {
   return {
     statusCode: 200,
     body: JSON.stringify({ message: 'Hello world from Netlify serverless function' }),
